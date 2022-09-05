d5c30a28423ac728ed10ed404b86b060bd6d5813
vjangid2507 <95203678+vjangid2507@users.noreply.github.com>
2022-09-05 17:16:01 +0530

    Update [index].js
    div added

--- pages/[index].js
+++ pages/[index].js
@@ -1,7 +1,11 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home({ data }) {
-  return <div className={styles.container}>{data.name}</div>;
+  return <div className={styles.container}>{data.name}
+  <div>Hello</div>
+  <div>How are You</div>
+  <div>I am fine</div>
+  </div>;
 }
 
 export async function getServerSideProps(ctx) {
